e4236579ecbe14bad8d525a143c8b3a3bc29b3b7
phil <phild@protonmail.com>
2017-09-30 14:35:26 +0200

    added matmul test with different dimensions

--- Pao/test/unit/test_mat.c
+++ Pao/test/unit/test_mat.c
@@ -40,36 +40,87 @@ static void test_matmult_2d(void **state){
 }
 
 static void test_matmult_3d(void **state){
-	#define d 3
-	float mat1[d][d] = {
+	float mat1[3][3] = {
 		{1.0, 2.0, 3.0},
 		{4.0, 5.0, 6.0},
 		{7.0, 8.0, 9.0}
 	};
-	float mat2[d][d] = {
+	float mat2[3][3] = {
 		{1.0, 0.0, 0.5},
 		{0.0, 1.0, 0.0},
 		{0.0, 0.0, 1.0}
 	};
-	float buffer[d][d] = {
+	float buffer[3][3] = {
 		{0.0, 0.0, 0.0},
 		{0.0, 0.0, 0.0},
 		{0.0, 0.0, 0.0}
 	};
-	float result[d][d] = {
+	float result[3][3] = {
 		{1.0, 2.0, 3.5},
 		{4.0, 5.0, 8.0},
 		{7.0, 8.0, 12.5}
 	};
 
-	mat_multiply(d,d,mat1,d,d,mat2,buffer);
-	for(int i = 0; i < d; i++){
-		for(int j = 0; j < d; j++){
+	mat_multiply(3,3,mat1,3,3,mat2,buffer);
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
 			assert_true(result[i][j] == buffer[i][j]);
+		}
+	}
+}
+
+static void test_matmult_bad(void **state){
+	float mat1[3][3] = {
+		{1.0, 2.0, 3.0},
+		{4.0, 5.0, 6.0},
+		{7.0, 8.0, 9.0}
+	};
+	float mat2[2][2] = {
+		{1.0, 0.0},
+		{0.0, 1.0}
+	};
+	float buffer[3][3] = {
+		{0.0, 0.0, 0.0},
+		{0.0, 0.0, 0.0},
+		{0.0, 0.0, 0.0}
+	};
+	
+
+	mat_multiply(3,3,mat1,2,2,mat2,buffer);
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			assert_true(buffer[i][j] == 0.0);
 		}
 	}
 } 
 
+static void test_matmult_3to2(void **state){
+	float mat1[3][3] = {
+		{1.0, 2.0, 3.0},
+		{4.0, 5.0, 6.0},
+		{7.0, 8.0, 9.0}
+	};
+	float mat2[3][2] = {
+		{1.0, 0.0},
+		{0.0, 1.0},
+		{0.0, 0.0}
+
+	};
+	float buffer[3][2] = {
+		{0.0, 0.0},
+		{0.0, 0.0},
+		{0.0, 0.0}
+	};
+	
+
+	mat_multiply(3,3,mat1,3,2,mat2,buffer);
+
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 2; j++){
+			assert_true(buffer[i][j] == mat1[i][j]);
+		}
+	}
+}
 /*
 * Register Tests
 */
@@ -78,6 +129,12 @@ int main(void)
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_linking),
 		cmocka_unit_test(test_matmult_2d),
+		cmocka_unit_test(test_matmult_3d),
+		cmocka_unit_test(test_matmult_bad),
+		cmocka_unit_test(test_matmult_3to2),
+
+
+
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
