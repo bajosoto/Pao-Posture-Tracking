98cf25b37d8e17fc7c542df8bf633a7218ef5905
Sergio <sergio.soto@live.com>
2017-10-03 18:51:50 +0200

    add transition to main screen

--- App/iOS/Pao-iOS/Pao-iOS/Controller/BleConnectVC.swift
+++ App/iOS/Pao-iOS/Pao-iOS/Controller/BleConnectVC.swift
@@ -14,16 +14,20 @@ class BleConnectVC: UIViewController, UITextViewDelegate, bleConnectionResponder
     
     @IBOutlet weak var loadingView: UIView!
     @IBOutlet weak var scanStatusLbl: UILabel!
+    @IBOutlet weak var spinnningIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var fadeOut: UIView!
     // IB outlet to Pao Finding View
     @IBOutlet weak var paoEggView: PaoFindingView!
     // Timer we'll use to refresh the Pao Finding animation
     fileprivate var clockTimer : Timer!
     // Ble Connection holding the connection to find Pao
+    var isFadingOut = false
     var bleConn: BleConnection?
     
     override func viewDidAppear(_ animated: Bool) {
         
         super.viewDidAppear(animated)
+        
         // Create a ble connection
         if let bleConnResp = self as bleConnectionResponder? {
             bleConn = BleConnection(responder: bleConnResp)
@@ -31,7 +35,7 @@ class BleConnectVC: UIViewController, UITextViewDelegate, bleConnectionResponder
         // Initialize the BT connection
         bleConn?.connectBlePao()
         
-        // Setup the Pao Finding animation timer
+        // Setup the Pao Finding animation timer @ 30Hz
         clockTimer = Timer.scheduledTimer( timeInterval: 0.03, target: self, selector: #selector(BleConnectVC.advancePaoFindingAnimation), userInfo: nil, repeats: true)
     }
     
@@ -45,12 +49,21 @@ class BleConnectVC: UIViewController, UITextViewDelegate, bleConnectionResponder
     // Advance the finding pao animation
     @objc func advancePaoFindingAnimation() {
         // No need to call setNeedsDisplay() since we do that in the setter for animationProgress
-        paoEggView.animationProgress += 0.015
+        paoEggView.animationProgress += 0.015 // (1/0.015) * 30ms ~= 2s egg oscillation
+        if isFadingOut {
+            if fadeOut.alpha < 1 {
+                fadeOut.alpha += 0.025  // (1/0.025) * 30ms = 1.2s transition
+            } else {
+                // Perform segue
+            }
+        }
     }
     
     func onPaoFound() {
-        print("A pao was found! ============================")
+        // print("A pao was found! ============================")
         scanStatusLbl.text = "Pao found! :)"
+        spinnningIndicator.isHidden = true
+        isFadingOut = true
     }
     
 }
