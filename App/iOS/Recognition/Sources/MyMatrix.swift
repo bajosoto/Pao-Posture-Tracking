7697e69b39be433b54580cb71365694c13df274b
phil <phild@protonmail.com>
2017-10-30 14:37:12 +0100

    moved static methods to MatrixMath class

--- App/iOS/Recognition/Sources/MyMatrix.swift
+++ App/iOS/Recognition/Sources/MyMatrix.swift
@@ -65,84 +65,16 @@ class MyMatrix : CustomStringConvertible{
 	}
 
 	func plus(_ that: MyMatrix) throws -> MyMatrix {
-		return try MyMatrix.plus(self,that)
+		return try MatrixMath.plus(self,that)
 	}
 
 	func minus(_ that: MyMatrix) throws -> MyMatrix {
-		return try MyMatrix.minus(self,that)
+		return try MatrixMath.minus(self,that)
 	}
 
 	func times(_ that: MyMatrix) throws -> MyMatrix {
-		return try MyMatrix.times(self,that)
+		return try MatrixMath.times(self,that)
 
 	}
-	static func plus(_ this: MyMatrix, _ that: MyMatrix) throws -> MyMatrix {
-
-		try checkDimAdd(this,that)
-		return doPlus(this,that)
-		
-	}
-
-	static func minus(_ this: MyMatrix, _ that: MyMatrix) throws -> MyMatrix {
-
-		try checkDimAdd(this,that)
-		return doMinus(this,that)
-		
-	}
-
-	static func times(_ this: MyMatrix, _ that: MyMatrix) throws -> MyMatrix {
-		try checkDimTimes(this,that)
-		return doTimes(this,that)
 	
-	}
-	internal static func checkDimAdd(_ this: MyMatrix, _ that: MyMatrix) throws{
-		if(this.rows != that.rows){
-			throw MatrixMathError.dimensionsDoNotMatch(this.rows,that.rows)
-		}
-
-		if(this.rows != that.rows){
-			throw MatrixMathError.dimensionsDoNotMatch(this.columns,that.columns)
-		}
-	}
-
-	internal static func checkDimTimes(_ this: MyMatrix, _ that: MyMatrix) throws{
-		if(this.columns != that.rows){
-			throw MatrixMathError.dimensionsDoNotMatch(this.columns,that.rows)
-		}		
-	}
-
-	internal static func doPlus(_ this: MyMatrix,_ that: MyMatrix) -> MyMatrix {
-		let out = MyMatrix(this.rows,this.columns)
-
-		for i in 0 ..< this.rows {
-			for j in 0 ..< this.columns {
-				out[i,j] = this[i,j] + that[i,j]
-			}
-		}
-		return out
-	}
-
-	internal static func doMinus(_ this: MyMatrix, _ that: MyMatrix) -> MyMatrix{
-		let out = MyMatrix(this.rows,this.columns)
-
-		for i in 0 ..< this.rows {
-			for j in 0 ..< this.columns {
-				out[i,j] = this[i,j] - that[i,j]
-			}
-		}
-		return out
-	}
-
-	internal static func doTimes(_ this: MyMatrix, _ that: MyMatrix) -> MyMatrix {
-		let out = MyMatrix(this.rows,that.columns)
-
-		for i in 0 ..< this.rows {
-			for j in 0 ..< that.columns {
-				for k in 0 ..< that.rows {
-					out[i,j] += this[i,k] * that[k,j]
-				}
-			}
-		}
-		return out
-	}
 }
