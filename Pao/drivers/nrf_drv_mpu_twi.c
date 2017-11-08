717e6c3c211e49fb00cdd2f2e773a7f1f2d79ffa
Sergio <sergio.soto@live.com>
2017-11-08 21:35:51 +0100

    change TWI pin to 7 and 30

--- Pao/drivers/nrf_drv_mpu_twi.c
+++ Pao/drivers/nrf_drv_mpu_twi.c
@@ -20,8 +20,10 @@
 #define MPU_TWI_SCL_PIN 3
 #define MPU_TWI_SDA_PIN 4
 #else
-#define MPU_TWI_SCL_PIN 1
-#define MPU_TWI_SDA_PIN 2
+// #define MPU_TWI_SCL_PIN 1        // Sergio: From what I see in the schematic, these are 7 and 30...
+// #define MPU_TWI_SDA_PIN 2
+#define MPU_TWI_SCL_PIN 7
+#define MPU_TWI_SDA_PIN 30
 #endif
 
 
