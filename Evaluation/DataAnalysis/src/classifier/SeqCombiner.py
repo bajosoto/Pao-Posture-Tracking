87a2567a3f2af0879c7e9b6d2ca38a47dd97cddf
phil <phild@protonmail.com>
2017-11-11 23:51:08 +0100

    added test for seq combiner

--- Evaluation/DataAnalysis/src/classifier/SeqCombiner.py
+++ Evaluation/DataAnalysis/src/classifier/SeqCombiner.py
@@ -3,16 +3,16 @@ from numpy.linalg import norm
 from sklearn.base import BaseEstimator
 
 
-class WeightedSeq:
+class SeqCombiner:
     def __init__(self, clf: BaseEstimator):
         self.clf = clf
         self.classes = 2
 
     def predict(self, sample: np.array):
         probas = np.zeros((1, self.classes))
-        for i in sample.shape[0]:
-            probas += i * 1 / sample.shape[0] * self.clf.predict(sample[i, :])
+        for i in range(sample.shape[0]):
+            probas += i * 1 / sample.shape[0] * self.clf.predict_proba(sample[i, :].reshape(1,-1))
 
         probas /= norm(probas)
 
-        return probas
+        return np.argmax(probas)
