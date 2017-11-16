0acbc68224a7e87c23fcc2887b3ccab666913962
Sergio <sergio.soto@live.com>
2017-11-16 22:09:26 +0100

    change magnitudeThetaPreprocessor, using theta, phi and magnitude

--- App/iOS/Recognition/Sources/PaoKnnClassifier.swift
+++ App/iOS/Recognition/Sources/PaoKnnClassifier.swift
@@ -1,7 +1,7 @@
 public class PaoKnnClassifier : BasePaoClassifier{
 	
-	public convenience init(_ traindata: [IPostureEntry],windowSize: Int = 10, kNeighbours: Int = 5){
-		self.init(traindata,KnnClassifier(kNeighbours:kNeighbours),SimplePreprocessor(windowSize))
+	public convenience init(_ traindata: [IPostureEntry],windowSize: Int = 10, kNeighbours: Int = 7){
+		self.init(traindata,KnnClassifier(kNeighbours:kNeighbours),MagnitudeThetaPreprocessor(windowSize))
 	}
 
 	
