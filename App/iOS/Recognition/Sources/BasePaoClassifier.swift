0413d5b64f017e1ed13680fe2b4a982c1ef7387d
Sergio <sergio.soto@live.com>
2017-11-16 18:01:34 +0100

    fix build issues

--- App/iOS/Recognition/Sources/BasePaoClassifier.swift
+++ App/iOS/Recognition/Sources/BasePaoClassifier.swift
@@ -51,25 +51,25 @@ public class  BasePaoClassifier: PaoClassifier {
 			}
 
 
-		/* apply preprocessing*/
-		var filtered: ([Vectorizable],[Int]) = (rawData,labels)
-		if preprocessor != nil{
-			let preprocessed = (try! preprocessor!.preprocess(rawData,labels))
-			filtered.0 = preprocessed.0.map{$0 as Vectorizable}
-			filtered.1 = preprocessed.1
-		}
+            /* apply preprocessing*/
+            var filtered: ([Vectorizable],[Int]) = (rawData,labels)
+            if preprocessor != nil{
+                let preprocessed = (try! preprocessor!.preprocess(rawData,labels))
+                filtered.0 = preprocessed.0.map{$0 as Vectorizable}
+                filtered.1 = preprocessed.1
+            }
 
-		/* convert to matrix */
-		var matrixRep = Matrix(filtered.0)
-		
-		/* apply scaling*/
-		self.scaler?.train(matrixRep)
-		matrixRep = scaler?.transform(matrixRep) ?? matrixRep
+            /* convert to matrix */
+            var matrixRep = Matrix(filtered.0)
+            
+            /* apply scaling*/
+            self.scaler?.train(matrixRep)
+            matrixRep = scaler?.transform(matrixRep) ?? matrixRep
 
-		/*generate trainingset and train*/
-		let trainset = try! Dataset(matrixRep,filtered.1)
-		self.classifier.train(trainset)
-
+            /*generate trainingset and train*/
+            let trainset = try! Dataset(matrixRep,filtered.1)
+            self.classifier.train(trainset)
+        }
 	}
 
 	public func predictSampleSoft(_ testdata: [IPostureEntry]) -> [IPostureEntry]{
@@ -81,81 +81,47 @@ public class  BasePaoClassifier: PaoClassifier {
 				rawData.append(RawSample(testdata[i]))
 			}
 
-			/* apply preprocessing -> average filter and peak2peak*/
-			let featureVector = try! preprocessor.preprocess(rawData)
+            /* apply preprocessing -> average filter and peak2peak*/
+            var featureVector:[Vectorizable] = rawData
+            if preprocessor != nil {
+            featureVector = try! preprocessor!.preprocess(rawData).map{$0 as Vectorizable}
 
-			/* apply same scaling as on the training set*/
-			let sampleMatrix = scaler.transform(Matrix(featureVector))
+            }
 
-			/* get the hard labels*/
-			let hardLabels = classifier.predict(samples:sampleMatrix)
-			
-			/* get the soft labels*/
-			let softLabels = classifier.predictSoft(samples:sampleMatrix)
+            /* apply same scaling as on the training set*/
+            let sampleMatrix = scaler?.transform(Matrix(featureVector)) ?? Matrix(featureVector)
 
-			/* create output data type for each classified sample*/
-			var predictions = [Predicition]()
-			for i in 0 ..< softLabels.count{
+            /* get the hard labels*/
+            var hardLabels = classifier.predict(samples:sampleMatrix)
 
-				/* calculate percentage of healthy/unhealthy*/
-				var probabilities = softLabels[i]
-				var badPosture = 0.0
-				var goodPosture = 0.0
-				if let sitNok = probabilities[Int(lookupLabel("SitNok"))]{
-					badPosture += sitNok
-				} 
-				if let standNok = probabilities[Int(lookupLabel("StandNok"))]{
-					badPosture += standNok
-				}
-				if let movNok = probabilities[Int(lookupLabel("MovNok"))]{
-					badPosture += movNok
-				}
-				if let sitOk = probabilities[Int(lookupLabel("SitOk"))]{
-					goodPosture += sitOk
-				}
-				if let standOk = probabilities[Int(lookupLabel("StandOk"))]{
-					goodPosture += standOk
-				}
-				if let movOk = probabilities[Int(lookupLabel("MovOk"))]{
-					goodPosture += movOk
-				}
+            hardLabels = try! postprocessor?.postprocess(hardLabels) ?? hardLabels
+            
+            /* get the soft labels*/
+            var softLabels = classifier.predictSoft(samples:sampleMatrix)
 
-		/* apply preprocessing -> average filter and peak2peak*/
-		var featureVector:[Vectorizable] = rawData
-		if preprocessor != nil {
-		featureVector = try! preprocessor!.preprocess(rawData).map{$0 as Vectorizable}
+            softLabels = try! postprocessor?.postprocess(predictionSoft:softLabels) ?? softLabels
 
-		}
+            /* create output data type for each classified sample*/
+            var predictions = [Predicition]()
+            for i in 0 ..< softLabels.count{
 
-		/* apply same scaling as on the training set*/
-		let sampleMatrix = scaler?.transform(Matrix(featureVector)) ?? Matrix(featureVector)
+                /* calculate percentage of healthy/unhealthy*/
+                var probabilities = softLabels[i]
+                var badPosture = probabilities[Int(lookupLabel("SitNok"))]!
+                badPosture += probabilities[Int(lookupLabel("StandNok"))]!
+                badPosture += probabilities[Int(lookupLabel("MovNok"))]!
+                var goodPosture = probabilities[Int(lookupLabel("SitOk"))]!
+                goodPosture += probabilities[Int(lookupLabel("StandOk"))]!
+                goodPosture += probabilities[Int(lookupLabel("MovOk"))]!
 
-		/* get the hard labels*/
-		var hardLabels = classifier.predict(samples:sampleMatrix)
+                /* put it all together */
+                predictions.append(Predicition(goodPosture-badPosture,lookupLabel(Double(hardLabels[i]))))
 
-		hardLabels = try! postprocessor?.postprocess(hardLabels) ?? hardLabels
-		
-		/* get the soft labels*/
-		var softLabels = classifier.predictSoft(samples:sampleMatrix)
-
-		softLabels = try! postprocessor?.postprocess(predictionSoft:softLabels) ?? softLabels
-
-		/* create output data type for each classified sample*/
-		var predictions = [Predicition]()
-		for i in 0 ..< softLabels.count{
-
-			/* calculate percentage of healthy/unhealthy*/
-			var probabilities = softLabels[i]
-			var badPosture = probabilities[Int(lookupLabel("SitNok"))]! 
-			badPosture += probabilities[Int(lookupLabel("StandNok"))]!
-			badPosture += probabilities[Int(lookupLabel("MovNok"))]!
-			var goodPosture = probabilities[Int(lookupLabel("SitOk"))]!
-			goodPosture += probabilities[Int(lookupLabel("StandOk"))]!
-			goodPosture += probabilities[Int(lookupLabel("MovOk"))]!
-
-			/* put it all together */
-			predictions.append(Predicition(goodPosture-badPosture,lookupLabel(Double(hardLabels[i]))))
-
-		}
+            }
+            
+            return predictions
+        } else {
+            return [Predicition]()
+        }
 	}
 }
