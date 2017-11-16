6237b64c7878bacd8523c3b30947b70e082d1616
phil <phild@protonmail.com>
2017-11-16 12:53:55 +0100

    soft label window passing

--- App/iOS/Recognition/Tests/RecognitionTests/PostMajorityVoteTest.swift
+++ App/iOS/Recognition/Tests/RecognitionTests/PostMajorityVoteTest.swift
@@ -12,10 +12,24 @@ class PostMajorityVoteTest: XCTestCase {
         pp = PostMajorityVote(5)
         XCTAssertEqual(try! pp.postprocessWindow(predictions),1)
     }
+
+    func testSoftLabel(){
+      var predictions = [[1: 0.4,
+                         2: 0.6],
+                         [1: 0.3,
+                         2: 0.7]]
+      var pp = PostMajorityVote(2)
+      var res = try! pp.postprocessWindow(predictionSoft:predictions)
+      XCTAssertEqual(res[1]!,Double(0.35))
+//      XCTAssertEqual(res[2]!,0.65)
+
+    }
         
      static var allTests : [(String, (PostMajorityVoteTest) -> () throws -> Void)] {
         return [
           ("testHardLabel",testHardLabel),
+          ("testSoftLabel",testSoftLabel),
+
         ]
     }
 }
