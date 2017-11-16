8749dda178f1899f2bf7cf67684f447cc034ca5a
phil <phild@protonmail.com>
2017-11-16 14:39:13 +0100

    default knn with PostMajorityVote

--- App/iOS/Recognition/Sources/PaoKnnClassifier.swift
+++ App/iOS/Recognition/Sources/PaoKnnClassifier.swift
@@ -1,7 +1,7 @@
 public class PaoKnnClassifier : BasePaoClassifier{
 	
-	public convenience init(_ traindata: [IPostureEntry],windowSize: Int = 10, kNeighbours: Int = 10){
-		self.init(traindata,KnnClassifier(kNeighbours:kNeighbours),SimplePreprocessor(10))
+	public convenience init(_ traindata: [IPostureEntry],windowSize: Int = 10, kNeighbours: Int = 5){
+		self.init(traindata,KnnClassifier(kNeighbours:kNeighbours),EqualScaler(),PostMajorityVote(windowSize))
 	}
 
 	
