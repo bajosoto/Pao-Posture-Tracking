9f6f6472595a41962d9f2e24571cf8c09fef06fc
phil <phild@protonmail.com>
2017-11-16 12:58:24 +0100

    added test for sequence post processing

--- App/iOS/Recognition/Tests/RecognitionTests/PostMajorityVoteTest.swift
+++ App/iOS/Recognition/Tests/RecognitionTests/PostMajorityVoteTest.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import Recognition
 class PostMajorityVoteTest: XCTestCase {
 
-    func testHardLabel(){
+    func testHardLabelWindow(){
         var predictions = [1,2,2]
         var pp = PostMajorityVote(3)
         XCTAssertEqual(try! pp.postprocessWindow(predictions),2)
@@ -13,7 +13,7 @@ class PostMajorityVoteTest: XCTestCase {
         XCTAssertEqual(try! pp.postprocessWindow(predictions),1)
     }
 
-    func testSoftLabel(){
+    func testSoftLabelWindow(){
       var predictions = [[1: 0.4,
                          2: 0.6],
                          [1: 0.3,
@@ -24,12 +24,41 @@ class PostMajorityVoteTest: XCTestCase {
 //      XCTAssertEqual(res[2]!,0.65)
 
     }
+
+    func testHardLabel(){
+        var predictions = [1,2,2,1,1,2]
+        var pp = PostMajorityVote(3)
+        XCTAssertEqual(try! pp.postprocess(predictions),[2,1])
+
+        predictions = [1,1,1,2,2,2,2,2,1,1]
+        pp = PostMajorityVote(5)
+        XCTAssertEqual(try! pp.postprocess(predictions),[1,2])
+    }
+
+    func testSoftLabel(){
+      var predictions = [[1: 0.4,
+                         2: 0.6],
+                         [1: 0.3,
+                         2: 0.7],
+                         [1: 0.6,
+                         2: 0.4],
+                         [1: 0.7,
+                         2: 0.3]]
+      var pp = PostMajorityVote(2)
+      var res = try! pp.postprocess(predictionSoft:predictions)
+      XCTAssertEqual(res[0][1]!,Double(0.35))
+//      XCTAssertEqual(res[2]!,0.65)
+      XCTAssertEqual(res[1][1]!,Double(0.65))
+
+
+    }
         
      static var allTests : [(String, (PostMajorityVoteTest) -> () throws -> Void)] {
         return [
+          ("testHardLabelWindow",testHardLabelWindow),
+          ("testSoftLabelWindow",testSoftLabelWindow),
           ("testHardLabel",testHardLabel),
           ("testSoftLabel",testSoftLabel),
-
         ]
     }
 }
