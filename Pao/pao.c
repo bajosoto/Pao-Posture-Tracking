7c9bf264bd86c5438e95d6ee44d79894175bda21
odt <ozantuna@hotmail.com>
2017-12-17 20:50:21 +0100

    Main.c now on par with the latest.

--- Pao/pao.c
+++ Pao/pao.c
@@ -1,18 +1,24 @@
 
 #include "pao.h"
+#include "flash-interface.h"
+#include "sm.h"
 
-//static uint32_t const writesomething = 1337;
-static uint32_t m_deadbeef[2] = {0xDEADBEEF,0xBAADF00D};
-#define TRY_FILE_ID     0x1111
-#define TRY_REC_KEY     0x2222
+#define TIMER_PERIOD_MS         5
+#define TIMER5_TIMER_PERIOD     APP_TIMER_TICKS(TIMER_PERIOD_MS, 0)  // timer period is in ms
+
 int programRunning = 1;
+uint16_t timer = 0;
+volatile uint8_t sync_timer = 1;
+
+void timer_5ms_handler() {
+    sync_timer = 0;
+}
 
 /**
  * @brief Function for application main entry.
  */
 int main(void)
 {
-    uint16_t timer = 0;
     uint32_t err_code;
 
     /* Configure board. */
@@ -24,15 +30,15 @@ int main(void)
     init_vibrator();
 
     //NRF_LOG_INIT(NULL);  // TODO_SERGIO: Using LOG to find RAM start and end address
-    
+
     // Old mpu init:
     //mpu_setup();
 
-    //nrf_delay_ms(2000);
+    // nrf_delay_ms(2000);
 
     // New mpu init:
-    // twi_init();
-    // imu_init(true, 100);
+    twi_init();
+    imu_init(true, 100);
 
     // Bluetooth
     app_timer_init_sergio();
@@ -41,162 +47,123 @@ int main(void)
     services_init();
     advertising_init();
     conn_params_init();
-    ret_code_t ret = fds_register(fds_evt_handler);
-    if (ret == FDS_SUCCESS)
-    {
-        debugMsg("Registering fds_evt_handler successful \n\r");
-    }
-    if (ret != FDS_SUCCESS)
-    {
-        debugMsg("Registering fds_evt_handler failed \n\r");
-    }
-    fds_init();
-    nrf_delay_ms(2000);
-    fds_data_write(TRY_FILE_ID,TRY_REC_KEY,m_deadbeef,2);
-    nrf_delay_ms(2000);
-    uint32_t *readthing = (uint32_t *) fds_data_read(TRY_FILE_ID, TRY_REC_KEY);
-
-    for (uint16_t i=0;i<2;i++)
-        {
-           debugMsg("In main, word number %d is %08x", i,readthing[i]);
-        }
-
-    entry_t writeentry;
-
-    writeentry.proba = (proba_t)1.5;
-    writeentry.label = 1;
-    writeentry.timestamp = 1000;
-
-    entry_t writeentry2;
-
-    writeentry2.proba = (proba_t)2.5;
-    writeentry2.label = 2;
-    writeentry2.timestamp = 2000;
-
-    entry_t writeentry3;
-
-    writeentry3.proba = (proba_t)3.5;
-    writeentry3.label = 3;
-    writeentry3.timestamp = 3000;
-    
-    cqInitialize();
-    nrf_delay_ms(3000);
-
-    cqEnqueue(&writeentry);
-    nrf_delay_ms(3000);
-
-    cqEnqueue(&writeentry2);
-    nrf_delay_ms(3000);
-
-    entry_t *readentry = (entry_t*)malloc(sizeof(entry_t));
-    readentry = cqDequeue();
-    nrf_delay_ms(3000);
-
-    debugMsg("Reading the entry, proba:%.2f",readentry->proba);
-    debugMsg("Reading the entry, label:%d",readentry->label);
-    debugMsg("Reading the entry, timestamp:%d",readentry->timestamp);
-
-    entry_t *readentry2 = (entry_t*)malloc(sizeof(entry_t));
-    readentry2 = cqDequeue();
-    nrf_delay_ms(3000);
-
-    debugMsg("Reading the entry, proba:%.2f",readentry2->proba);
-    debugMsg("Reading the entry, label:%d",readentry2->label);
-    debugMsg("Reading the entry, timestamp:%d",readentry2->timestamp);
-
-    entry_t *readentry3 = (entry_t*)malloc(sizeof(entry_t));
-    readentry3 = cqDequeue();
-    nrf_delay_ms(3000);
-    debugMsg("Reading the entry, label:%.2f",readentry3->label);
-
-    cqEnqueue(&writeentry);
-    nrf_delay_ms(3000);
-
-    cqEnqueue(&writeentry2);
-    nrf_delay_ms(3000);
-
-    cqEnqueue(&writeentry3);
-    nrf_delay_ms(3000);
-
-    entry_t *readentry4 = (entry_t*)malloc(sizeof(entry_t));
-    readentry4 = cqDequeue();
-    nrf_delay_ms(3000);
-
-    debugMsg("Reading the entry, proba:%.2f",readentry4->proba);
-    debugMsg("Reading the entry, label:%d",readentry4->label);
-    debugMsg("Reading the entry, timestamp:%d",readentry4->timestamp);
-
-    entry_t *readentry5 = (entry_t*)malloc(sizeof(entry_t));
-    readentry5 = cqDequeue();
-    nrf_delay_ms(3000);
-
-    debugMsg("Reading the entry, proba:%.2f",readentry5->proba);
-    debugMsg("Reading the entry, label:%d",readentry5->label);
-    debugMsg("Reading the entry, timestamp:%d",readentry5->timestamp);
-
-    entry_t *readentry6 = (entry_t*)malloc(sizeof(entry_t));
-    readentry6 = cqDequeue();
-    nrf_delay_ms(3000);
-
-    debugMsg("Reading the entry, proba:%.2f",readentry6->proba);
-    debugMsg("Reading the entry, label:%d",readentry6->label);
-    debugMsg("Reading the entry, timestamp:%d",readentry6->timestamp);
-
-    entry_t *readentry7 = (entry_t*)malloc(sizeof(entry_t));
-    readentry7 = cqDequeue();
-    debugMsg("Reading the entry, label:%.2f",readentry7->label);
-
 
     err_code = ble_advertising_start(BLE_ADV_MODE_FAST);
     APP_ERROR_CHECK(err_code);
 
-    /* Toggle LEDs. */
+    /* Enable Timer */
+    APP_TIMER_DEF(timer_5ms);
+    app_timer_create(&timer_5ms, APP_TIMER_MODE_REPEATED, timer_5ms_handler);
+    app_timer_start(timer_5ms, TIMER5_TIMER_PERIOD, NULL);
+
+    // /* Init Classifier stuff */
+    knn_init(KNN_NEIGHBORS);
+    transformer_t scalers[1] = {TRANSF_SCALE_STD};
+    clf_init(CLF_KNN, 1, scalers);
+
+    /* Main loop */
     while (programRunning) {
 
+        while (sync_timer);
+        sync_timer = 1;
+
         uint8_t cr;
-        
         if (app_uart_get(&cr) == NRF_SUCCESS){
             setrxByte(cr);
         }
-        // TODO: add ble
 
         if (check_sensor_int_flag()) {
-            //getMpuSensors();
+            getMpuSensors();
         }
 
-        if(timer % 50 == 0) {
-            if(bsp_board_button_state_get(3)) {
-                // sendMsgBle(16, "Button 3 Pressed");
-                debugMsgBle("Button pressed");
-                start_snooze();
+        // @ 100ms = 10Hz
+        if(timer % (1000 / (5 * SAMPLING_FREQ)) == 0) {  
+            sendMessageEs(MSG02_SENSOR_VALS);
+            // sendBleMessageEs(MSG_BLE_02_SENSOR);
+            
+            class_t label;
+            entry_t* newEntry = (entry_t*)malloc(sizeof(entry_t));
+
+            switch(smCurrState) {
+
+                case S2_DISCONNECTED:
+                    label = process_new_sample(CLASS_NO_CLASS, newEntry);
+                    if(label != CLASS_NO_CLASS) {
+                        // Store in Flash
+                        store_entry(newEntry);
+                    }
+                    break;
+
+                case S3_CONNECTED:
+                    label = process_new_sample(CLASS_NO_CLASS, newEntry);
+                    if(label != CLASS_NO_CLASS) {
+                        // Set timestamp to zero, since in connected mode results are real-time
+                        newEntry->timestamp = 0;
+                        // Send to App
+                        sendBleEntry(newEntry);
+
+                        //debugMsgBle("Lb: %d\tT: %d\tPr: %d", newEntry->label, newEntry->timestamp, (int32_t)(newEntry->proba * 100));
+                    }
+                    break;
+
+                case S4_TRAINING:
+                    process_new_sample(train_label, NULL);
+                    break;
+
+                case S5_DUMPING:
+                    if(data_dump()) {
+                        debugMsg("sent entry");
+                    } else {
+                        switchState(S3_CONNECTED);
+                    }
+                    break;
+                default:
+                    // Do nothing
+                    break;
             }
-            // for(int i = 0; i < BUTTONS_NUMBER; i++) {
-            //     if(bsp_board_button_state_get(i)) {
-            //         printf("Pressed Button %d\r\n", i);
-            //         bsp_board_led_invert(i);
+
+            free(newEntry);
+
+            // if(smCurrState == S4_TRAINING) {
+            //     // Add sample to training data TODO
+            //     process_new_sample(train_label);        // TODO Get this from BLE!
+            // } else if(smCurrState == S2_DISCONNECTED || smCurrState == S3_CONNECTED) {
+            //     class_t label = process_new_sample(CLASS_NO_CLASS);
+            //     // If there was a classification
+            //     if(label != CLASS_NO_CLASS) {
+            //         // Store in flash (TODO)
+            //         // 
+            //         if(smCurrState == S3_CONNECTED) {
+            //             // Send to phone (TODO)
+            //             // sendBleMessageEs(SOME_NEW_MESSAGE);
+            //         }
             //     }
             // }
         }
-        if(timer % 10 == 0) {  // Every 100ms  errr 50
-            // getMpuSensors();
-            sendMessageEs(MSG02_SENSOR_VALS);
-            sendBleMessageEs(MSG_BLE_02_SENSOR);            
-        }
-        // if(timer % 50 == 0) {
-        //     sendBleMessageEs(MSG_BLE_02_SENSOR);
-        // }
 
+        // @ 250ms
+        if(timer % 50 == 0) {
+            if(bsp_board_button_state_get(3)) {
+                debugMsgBle("Button 3 pressed");
+                start_snooze();
+            }
+        }
+
+        // @ 500ms
         if(timer % 100 == 0) {
-            // getPedo();                                   // For now I disabled pedo
+            // For now I disabled pedo
+            // getPedo();                       
             // sendBleMessageEs(MSG_BLE_04_PEDO);
             bsp_board_led_invert(0);
-            //buzz(10);        // Testing only
+        }
+
+        if(timer % 200 == 0) {
+            advance_second();
             timer = 0;
         }
 
-        increment_buzz_time(5);
+        increment_buzz_time(TIMER_PERIOD_MS);
 
-        nrf_delay_ms(5);
         timer++;
     }
 }
